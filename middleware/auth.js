6cd167fd914cfe3dafda6a15bc8a7224d0b3a629
Joshua <favrejoshua@yahoo.com>
2021-05-22 13:35:21 -0500

    add auth middleware to auth route

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = function (req, res, next) {
 
 	// Check if there is no token
 	if (!token) {
-		return res.status(401).json({ msg: 'NO token, authorization denied' });
+		return res.status(401).json({ msg: 'No token, authorization denied' });
 	}
 
 	// Verify token
