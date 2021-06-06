532907378f1af3ecfa2cd68c0ae5dcde7c7093a9
Joshua <favrejoshua@yahoo.com>
2021-06-06 17:00:20 -0500

    complete landing page and fix routes in navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Routes from './components/routing/Routes';
 import './App.css';
 
 const App = () => (
 	<Router>
 		<Fragment>
 			<Navbar />
-			<Route exact path='/' component={Landing} />
+			<Switch>
+				<Route exact path='/' component={Landing} />
+				<Route component={Routes} />
+			</Switch>
 		</Fragment>
 	</Router>
 );
