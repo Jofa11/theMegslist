1acf264b56f72458ff59823088f936d4f39e9201
Joshua <favrejoshua@yahoo.com>
2021-06-06 16:37:45 -0500

    add login and register forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import './App.css';
 
-const App = () => 
-    <Fragment>
-     <Navbar />
-     <Landing />
-    </Fragment>
+const App = () => (
+	<Router>
+		<Fragment>
+			<Navbar />
+			<Route exact path='/' component={Landing} />
+		</Fragment>
+	</Router>
+);
 
 
 export default App;
