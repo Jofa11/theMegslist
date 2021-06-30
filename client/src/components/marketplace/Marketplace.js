9bf2b7d1f9903643f4d03db571e1b521763f548c
Joshua <favrejoshua@yahoo.com>
2021-06-30 02:30:39 -0500

    create marketplace component and import in routes

---
+++ client/src/components/marketplace/Marketplace.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Marketplace = props => {
+    return (
+        <div>
+            Marketplace
+        </div>
+    )
+}
+
+Marketplace.propTypes = {
+
+}
+
+export default Marketplace
