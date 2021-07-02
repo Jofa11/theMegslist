8303d30394b2709b5f2b6a1824373074ca52fe56
Joshua <favrejoshua@yahoo.com>
2021-07-01 23:34:32 -0500

    add email to profile schema, create profile reducer file, and add to index

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,13 @@
+const initialState = {
+    profile: null,
+    loading: true,
+    error: {}
+}
+
+export default function(state = initialState, action) {
+    const { type, payload } = action;
+
+    switch(type) {
+        
+    }
+}
