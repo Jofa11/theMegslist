a8447de746b0cc91e9f3637ef152a583de88982f
Joshua <favrejoshua@yahoo.com>
2021-07-02 23:59:12 -0500

    add auth landing page links, add icon and user name to marketplace

--- client/src/components/marketplace/Marketplace.js
+++ client/src/components/marketplace/Marketplace.js
@@ -1,29 +1,41 @@
-import React, { useEffect } from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
+import { Fragment } from 'react';
 
-const Marketplace = ({ getCurrentProfile, auth, profile }) => {
-    useEffect(() => {
-        getCurrentProfile();
-    }, []);
+const Marketplace = ({
+	getCurrentProfile,
+	auth: { user },
+	profile: { profile, loading },
+}) => {
+	useEffect(() => {
+		getCurrentProfile();
+	}, []);
 
-    return (
-        <div>
-            Marketplace
-        </div>
-    )
-}
+	return loading && profile === null ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<h1 className='large text-primary'>Marketplace</h1>
+			<p className='lead'>
+				<i className='fas fa-user'></i> Welcome {user && user.name}
+			</p>
+            {profile !== null ? <Fragment></Fragment> : <Fragment></Fragment>}
+		</Fragment>
+	);
+};
 
 Marketplace.propTypes = {
-    getCurrentProfile: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    profile: PropTypes.object.isRequired,
-}
+	getCurrentProfile: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
-    auth: state.auth,
-    profile: state.profile
+const mapStateToProps = (state) => ({
+	auth: state.auth,
+	profile: state.profile,
 });
 
 export default connect(mapStateToProps, { getCurrentProfile })(Marketplace);
