22f6b8120fe1f7a61a394266f4f3c09871d18c9d
Joshua <favrejoshua@yahoo.com>
2021-07-06 23:11:55 -0500

    add posts route to routes

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -5,24 +5,20 @@ import Spinner from '../layout/Spinner';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
-    useEffect(() => {
-        getPosts();
-    }, [getPosts]);
+	useEffect(() => {
+		getPosts();
+	}, [getPosts]);
 
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return <div></div>;
+};
 
 Posts.propTypes = {
-    getPosts: PropTypes.func.isRequired,
-    post: PropTypes.object.isRequired,
-}
+	getPosts: PropTypes.func.isRequired,
+	post: PropTypes.object.isRequired,
+};
 
-const mapStateToProps = state => ({
-    post: state.post;
-})
+const mapStateToProps = (state) => ({
+	post: state.post,
+});
 
 export default connect(mapStateToProps, { getPosts })(Posts);
