288e76b5d83898241d8369d6cb916b3c7a51bd2c
Joshua <favrejoshua@yahoo.com>
2021-07-07 12:54:10 -0500

    post items are functional

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -16,30 +16,25 @@ const PostItem = ({
 					src='https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200'
 					alt=''
 				/>
-				<h4>John Doe</h4>
+				<h4>{name}</h4>
 			</a>
 		</div>
 		<div>
-			<p className='my-1'>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint possimus
-				corporis sunt necessitatibus! Minus nesciunt soluta suscipit nobis. Amet
-				accusamus distinctio cupiditate blanditiis dolor? Illo perferendis
-				eveniet cum cupiditate aliquam?
+			<p className='my-1'>{text}</p>
+			<p className='post-date'>
+				Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
 			</p>
-			<p className='post-date'>Posted on 04/16/2019</p>
-			<button type='button' className='btn btn-light'>
-				<i className='fas fa-thumbs-up'></i>
-				<span>4</span>
-			</button>
-			<button type='button' className='btn btn-light'>
-				<i className='fas fa-thumbs-down'></i>
-			</button>
-			<a href='post.html' className='btn btn-primary'>
-				Discussion <span className='comment-count'>2</span>
-			</a>
-			<button type='button' className='btn btn-danger'>
-				<i className='fas fa-times'></i>
-			</button>
+			<Link to={`/post/${_id}`} className='btn btn-primary'>
+				Comment{' '}
+				{comments.length > 0 && (
+					<span className='comment-count'>{comments.length}</span>
+				)}
+			</Link>
+			{!auth.loading && user === auth.user._id && (
+				<button type='button' className='btn btn-danger'>
+					<i className='fas fa-times'></i>
+				</button>
+			)}
 		</div>
 	</div>
 );
